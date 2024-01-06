85c7974dbf5f9bfbadd4d3d7749e000a0410b5f4
User <User@LAPTOP-1ND091FI>
2024-01-06 12:00:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        // Creating a list using ArrayList (non-generic)
+        IList myList = new ArrayList();
+
+        // Adding elements
+        myList.Add("Apple");
+        myList.Add("Banana");
+        myList.Add("Orange");
+
+        // Accessing elements using the indexer property
+        Console.WriteLine("Element at index 0: " + myList[0]);
+        Console.WriteLine("Element at index 1: " + myList[1]);
+        Console.WriteLine("Element at index 2: " + myList[2]);
+    }
+}
